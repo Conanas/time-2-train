ae35ff91c3c92e8b9874c08a78d7c7ae4955d736
Tom Haigh <tmhaigh@gmail.com>
2021-01-10 15:59:11 +1100

    removed unnecessary code

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
+import StartEditTimer from './pages';
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <>
+    </>
+  )
 }
-
-export default App;
