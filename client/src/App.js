cc302e4740c697eadb886ea9ce8d8421912bff0c
Tom Haigh <tmhaigh@gmail.com>
2021-01-10 16:03:11 +1100

    added browserRouter and react-dom-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import StartEditTimer from './pages';
 import "./App.css";
 
 export default function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Switch>
+        <Route path exact="/">
+          <StartEditTimer />
+        </Route>
+      </Switch>
+
+    </Router>
   )
 }
