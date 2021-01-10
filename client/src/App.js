8a2754c315ec3d43898b9d9d302d657009aa8dcf
Tom Haigh <tmhaigh@gmail.com>
2021-01-10 21:25:31 +1100

    importing startedittimer page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import StartEditTimer from './pages';
+import StartEditTimer from './pages/Start-Edit-Timer/';
+import Container from './components/Container/';
 import "./App.css";
 
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path exact="/">
-          <StartEditTimer />
-        </Route>
-      </Switch>
-
-    </Router>
-  )
+    return (
+        <Container>
+            <Router>
+                <Switch>
+                    <Route path exact="/">
+                        <StartEditTimer />
+                    </Route>
+                </Switch>
+            </Router>
+        </Container>
+    )
 }
