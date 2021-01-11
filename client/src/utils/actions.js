ef33df0c18b0e58188546a510e246bd515aa3170
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 21:24:10 +1100

    created const actions

---
+++ client/src/utils/actions.js
@@ -0,0 +1,7 @@
+export const SET_CONTINUOUS = "SET_CONTINUOUS";
+export const SET_PREPARE = "SET_PREPARE";
+export const SET_REPS = "SET_REPS";
+export const SET_WORK = "SET_WORK";
+export const SET_REST = "SET_REST";
+export const SET_SETS = "SET_SETS";
+export const SET_BREAK = "SET_BREAK";
