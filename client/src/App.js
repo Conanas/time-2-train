10bc7adb0a784c8e70ebb113022a94f0d02a29b5
Tom Haigh <tmhaigh@gmail.com>
2021-01-11 21:22:58 +1100

    added WorkoutProvider

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import StartEditTimer from './pages/Start-Edit-Timer/';
 import Container from './components/Container/';
+import { WorkoutProvider } from './utils/GlobalState';
 import "./App.css";
 
 export default function App() {
-    return (
-        <Container>
-            <Router>
-                <Switch>
-                    <Route path exact="/">
-                        <StartEditTimer />
-                    </Route>
-                </Switch>
-            </Router>
-        </Container>
-    )
+  return (
+    <Container>
+      <WorkoutProvider>
+        <Router>
+          <Switch>
+            <Route path exact="/">
+              <StartEditTimer />
+            </Route>
+          </Switch>
+        </Router>
+      </WorkoutProvider>
+    </Container>
+  )
 }
