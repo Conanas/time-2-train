6c104c558590066460734371ed5ae2e235a285ad
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 09:24:00 +1100

    created titles for list items

---
+++ client/src/utils/list-titles.js
@@ -0,0 +1,7 @@
+export const CONTINUOUS = "Continuous";
+export const PREPARE = "Prepare";
+export const REPS = "Reps";
+export const WORK = "Work";
+export const REST = "Rest";
+export const SETS = "Sets";
+export const BREAK = "Break";
