bea1f31c75cfc470aa8dc80b99e876319d354ae6
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 09:55:04 +1100

    added comments

--- client/src/pages/start-edit-timer/index.js
+++ client/src/pages/start-edit-timer/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { useWorkoutContext } from "../../utils/GlobalState";
 import {
-  SET_CONTINUOUS,
-  SET_PREPARE,
-  SET_REPS,
-  SET_WORK,
-  SET_REST,
-  SET_SETS,
-  SET_BREAK
-} from '../../utils/actions';
-import {
   CONTINUOUS,
   WORK
 } from '../../utils/list-titles';
@@ -20,24 +11,41 @@ export default function StartEditTimer() {
 
   function renderListItems(state) {
     const listItems = Object.keys(state).map(key => {
-      let inputs = <>
-        <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: state[key].action, payload: -1 })}></i>
-        <div className="workout-units flow-text">
-          <input type="number" value={state[key].value} />
-        </div>
-        <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: state[key].action, payload: +1 })}></i>
-      </>;
+
+      // The input elements JSX for each item in the state object
+      let inputs = (
+        <>
+          <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: state[key].action, payload: -1 })}></i>
+          <div className="workout-units flow-text">
+            <input type="number" value={state[key].value} />
+          </div>
+          <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: state[key].action, payload: +1 })}></i>
+        </>
+      )
+
+      // When we reach the continuous object in the state then render the continuous toggle button
+      // If continuous then render 'on' toggle switch
+      // Else render the 'off' toggle switch
       if (state[key].title === CONTINUOUS) {
         if (state[key].value === true) {
-          inputs = <i className="fa-icon fas fa-toggle-on flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: false })}></i>
+          inputs = (
+            <i className="fa-icon fas fa-toggle-on flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: false })}></i>
+          )
         } else {
-          inputs = <i className="fa-icon fas fa-toggle-off flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: true })}></i>
-        }
-      } else if (state[key].title === WORK) {
-        if (state.continuous.value === false) {
-          return (<></>)
+          inputs = (
+            <i className="fa-icon fas fa-toggle-off flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: true })}></i>
+          )
         }
       }
+
+      // When we reach the Work object in the state and continuous is set to false then do not render the work element
+      else if (state[key].title === WORK) {
+        if (state.continuous.value === false) {
+          return <></>
+        }
+      }
+
+      // Return each item with the title of each object in the state and with the inputs variable
       return (
         <li className="edit-timer-list-item">
           <label className="flow-text">{state[key].title}</label>
