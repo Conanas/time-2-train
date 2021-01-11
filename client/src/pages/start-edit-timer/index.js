80c86075bf28e979aa785f2c2a964600b3756278
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 09:44:52 +1100

    map list items over object working

--- client/src/pages/start-edit-timer/index.js
+++ client/src/pages/start-edit-timer/index.js
@@ -18,97 +18,41 @@ import './style.css';
 export default function StartEditTimer() {
   const [state, dispatch] = useWorkoutContext();
 
-  function renderContinuous(state) {
-    if (state.continuous.value === true) {
-      return <i className="fa-icon fas fa-toggle-on flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: false })}></i>
-    }
-    return <i className="fa-icon fas fa-toggle-off flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: true })}></i>
+  function renderListItems(state) {
+    const listItems = Object.keys(state).map(key => {
+      let inputs = <>
+        <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: state[key].action, payload: -1 })}></i>
+        <div className="workout-units flow-text">
+          <input type="number" value={state[key].value} />
+        </div>
+        <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: state[key].action, payload: +1 })}></i>
+      </>;
+      if (state[key].title === CONTINUOUS) {
+        if (state[key].value === true) {
+          inputs = <i className="fa-icon fas fa-toggle-on flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: false })}></i>
+        } else {
+          inputs = <i className="fa-icon fas fa-toggle-off flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: true })}></i>
+        }
+      } else if (state[key].title === WORK) {
+        if (state.continuous.value === false) {
+          return (<></>)
+        }
+      }
+      return (
+        <li className="edit-timer-list-item">
+          <label className="flow-text">{state[key].title}</label>
+          {inputs}
+        </li>
+      )
+    })
+    return listItems;
   }
 
   return (
     <>
       <h1 className="flow-text">Start/Edit Workout</h1>
       <ul className="edit-timer-list">
-
-        {Object.keys(state).map(item => {
-          return (
-            <li className="edit-timer-list-item">
-              <label className="flow-text">{state[item].title}</label>
-              {state[item].title === CONTINUOUS ? renderContinuous(state) :
-                <>
-                  <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: state[item].action, payload: -1 })}></i>
-                  <div className="workout-units flow-text">
-                    <input type="number" value={state[item].value} />
-                  </div>
-                  <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: state[item].action, payload: 1 })}></i>
-                </>
-              }
-            </li>
-          )
-        })}
-
-        {/* Prepare */}
-        <li className="edit-timer-list-item">
-          <label className="flow-text">Prepare</label>
-          <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: SET_PREPARE, payload: -1 })}></i>
-          <div className="workout-units flow-text">
-            <input type="number" value={state.prepare} />
-          </div>
-          <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: SET_PREPARE, payload: +1 })}></i>
-        </li>
-
-        {/* Reps */}
-        <li className="edit-timer-list-item">
-          <label className="flow-text">Reps</label>
-          <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: SET_REPS, payload: -1 })}></i>
-          <div className="workout-units flow-text">
-            <input type="number" value={state.reps} />
-          </div>
-          <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: SET_REPS, payload: +1 })}></i>
-        </li>
-
-        {/* Work */}
-        {state.continuous === true ?
-          <li className="edit-timer-list-item">
-            <label className="flow-text">Work</label>
-            <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: SET_WORK, payload: -1 })}></i>
-            <div className="workout-units flow-text">
-              <input type="number" value={state.work} />
-            </div>
-            <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: SET_WORK, payload: +1 })}></i>
-          </li> :
-          null}
-
-        {/* Rest */}
-        <li className="edit-timer-list-item">
-          <label className="flow-text">Rest</label>
-          <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: SET_REST, payload: -1 })}></i>
-          <div className="workout-units flow-text">
-            <input type="number" value={state.rest} />
-          </div>
-          <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: SET_REST, payload: +1 })}></i>
-        </li>
-
-        {/* Sets */}
-        <li className="edit-timer-list-item">
-          <label className="flow-text">Sets</label>
-          <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: SET_SETS, payload: -1 })}></i>
-          <div className="workout-units flow-text">
-            <input type="number" value={state.sets} />
-          </div>
-          <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: SET_SETS, payload: +1 })}></i>
-        </li>
-
-        {/* Break */}
-        <li className="edit-timer-list-item">
-          <label className="flow-text">Break</label>
-          <i className="fa-icon far fa-minus-square flow-text" onClick={() => dispatch({ type: SET_BREAK, payload: -1 })}></i>
-          <div className="workout-units flow-text">
-            <input type="number" value={state.break} />
-          </div>
-          <i className="fa-icon far fa-plus-square flow-text" onClick={() => dispatch({ type: SET_BREAK, payload: +1 })}></i>
-        </li>
-
+        {renderListItems(state)}
       </ul>
       <input className="form-button" type="button" value="Start"></input>
       <input className="form-button" type="button" value="Save"></input>
