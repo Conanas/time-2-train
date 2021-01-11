5691e5cf69ed84bf7a209b42460f8877c341c43a
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 10:00:32 +1100

    tidied up renderListItems function

--- client/src/pages/start-edit-timer/index.js
+++ client/src/pages/start-edit-timer/index.js
@@ -26,23 +26,16 @@ export default function StartEditTimer() {
       // When we reach the continuous object in the state then render the continuous toggle button
       // If continuous then render 'on' toggle switch
       // Else render the 'off' toggle switch
-      if (state[key].title === CONTINUOUS) {
-        if (state[key].value === true) {
-          inputs = (
-            <i className="fa-icon fas fa-toggle-on flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: false })}></i>
-          )
-        } else {
-          inputs = (
-            <i className="fa-icon fas fa-toggle-off flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: true })}></i>
-          )
-        }
+      if (state[key].title === CONTINUOUS && state[key].value === true) {
+        inputs = <i className="fa-icon fas fa-toggle-on flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: false })}></i>
+      }
+      if (state[key].title === CONTINUOUS && state[key].value === false) {
+        inputs = <i className="fa-icon fas fa-toggle-off flow-text" onClick={() => dispatch({ type: state.continuous.action, payload: true })}></i>
       }
 
       // When we reach the Work object in the state and continuous is set to false then do not render the work element
-      else if (state[key].title === WORK) {
-        if (state.continuous.value === false) {
-          return <></>
-        }
+      else if (state[key].title === WORK && state.continuous.value === false) {
+        return <></>
       }
 
       // Return each item with the title of each object in the state and with the inputs variable
