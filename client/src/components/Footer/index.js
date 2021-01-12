e8e4afc7dac4f1bfd90ee1df22d45e71bc1152b7
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 11:53:21 +1100

    added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-row row">
+          <div className="links-div col s6">
+            <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin font-awesome social-icon"></i>
+            </a>
+            <a className="links-div-anchor" href="https://github.com/Conanas" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github font-awesome social-icon"></i>
+            </a>
+          </div>
+          <div className="contact-div col s6">
+            <label>Phone: 0406 253 030</label>
+            <label>Email: tmhaigh@gmail.com</label>
+            <label>Location: Melbourne, Victoria, Australia</label>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
