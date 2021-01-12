bc8f0f0fa221bbfd6cfb397694e81e558b81bf69
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 17:38:59 +1100

    changed class to className

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,16 +5,16 @@ export default function Navbar() {
   return (
     <>
       <nav>
-        <div class="nav-wrapper container">
-          <a href="#!" class="brand-logo right">Workout Time</a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="left hide-on-med-and-down">
+        <div className="nav-wrapper container">
+          <a href="#!" className="brand-logo right">Workout Time</a>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <ul className="left hide-on-med-and-down">
             <li><a className="nav-item" href="/">Start/Edit Timer</a></li>
           </ul>
         </div>
       </nav>
 
-      <ul class="sidenav" id="mobile-demo">
+      <ul className="sidenav" id="mobile-demo">
         <li><a href="/">Start/Edit Timer</a></li>
       </ul>
     </>
