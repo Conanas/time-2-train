6c2771c522a75517f78397fe893de302ec0f944f
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 16:00:06 +1100

    added logger and routes path

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
+const routes = require("./routes/");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(logger("dev"));
+
+// Add routes
+app.use(routes);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true
 });
 
-app.use(logger("dev"));
-
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
