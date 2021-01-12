9a71194495deec2b4a3ba1524985cfbe4ab6c6ae
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 14:11:07 +1100

    added mongoose connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,12 +10,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true
+});
+
+app.use(logger("dev"));
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
