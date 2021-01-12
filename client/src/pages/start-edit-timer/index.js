282932b23f6a73ace9e42755c2a372fca4ffbd46
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 20:06:51 +1100

    divided up buttons and from into divs for flex

--- client/src/pages/start-edit-timer/index.js
+++ client/src/pages/start-edit-timer/index.js
@@ -18,13 +18,17 @@ export default function StartEditTimer() {
 
   return (
     <>
-      <h1 className="flow-text">Start/Edit Workout</h1>
-      <ul className="edit-timer-list">
-        <ListItems />
-      </ul>
-      <input className="form-button" type="button" value="Start"></input>
-      <input className="form-button" type="button" value="Save"></input>
-      <input className="form-button" type="button" value="Load"></input>
+      <div>
+        <h1 className="flow-text">Start/Edit Workout</h1>
+        <ul className="edit-timer-list">
+          <ListItems />
+        </ul>
+      </div>
+      <div className="button-div">
+        <input className="form-button" type="button" value="Start"></input>
+        <input className="form-button" type="button" value="Save"></input>
+        <input className="form-button" type="button" value="Load"></input>
+      </div>
     </>
   )
 }
