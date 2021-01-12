b6ce38c5c0cf3f15134092cf56bc5cf9732dca8c
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 11:53:03 +1100

    deleted container component

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import StartEditTimer from './pages/Start-Edit-Timer/';
-import Container from './components/Container/';
+import Footer from './components/Footer/';
 import { WorkoutProvider } from './utils/GlobalState';
 import "./App.css";
 
 export default function App() {
   return (
-    <Container>
-      <WorkoutProvider>
-        <Router>
-          <Switch>
-            <Route path exact="/">
-              <StartEditTimer />
-            </Route>
-          </Switch>
-        </Router>
-      </WorkoutProvider>
-    </Container>
+    <div className="wrapper">
+      <main className="container">
+        <WorkoutProvider>
+          <Router>
+            <Switch>
+              <Route path exact="/">
+                <StartEditTimer />
+              </Route>
+            </Switch>
+          </Router>
+        </WorkoutProvider>
+      </main>
+      <Footer />
+    </div>
   )
 }
