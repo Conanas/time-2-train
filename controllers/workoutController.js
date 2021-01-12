96d482189cab4f1657dd20168e4523d09062f997
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 16:47:15 +1100

    findbyid by req.params.id

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   findById: function (req, res) {
     db.Workout
-      .findById({ title: req.params.id })
+      .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
