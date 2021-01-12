4a47de103c4372a1b24c336e9aee72b1097049a1
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 17:46:19 +1100

    removed old actions

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,14 +1,3 @@
-export const SET_CONTINUOUS = "SET_CONTINUOUS";
-export const SET_PREPARE = "SET_PREPARE";
-export const SET_REPS = "SET_REPS";
-export const SET_WORK = "SET_WORK";
-export const SET_REST = "SET_REST";
-export const SET_SETS = "SET_SETS";
-export const SET_BREAK = "SET_BREAK";
-
-export const SET_WORKOUT = "SET_WORKOUT";
-export const SET_TITLE = "SET_TITLE";
-
 export const SET_ACTIONS = {
   continuous: "SET_CONTINUOUS",
   prepare: "SET_PREPARE",
