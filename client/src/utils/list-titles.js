57478f230201121b7fc7d5a8d7069d3c71eaf3a7
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 17:40:04 +1100

    added new TITLES objects

--- client/src/utils/list-titles.js
+++ client/src/utils/list-titles.js
@@ -6,3 +6,14 @@ export const WORK = "Work";
 export const REST = "Rest";
 export const SETS = "Sets";
 export const BREAK = "Break";
+
+export const TITLES = {
+  continuous: "Continuous",
+  prepare: "Prepare",
+  reps: "Reps",
+  work: "Work",
+  rest: "Rest",
+  sets: "Sets",
+  break: "Break",
+  title: "Title"
+}
