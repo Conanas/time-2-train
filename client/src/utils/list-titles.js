e780ac105eef9a3f7a48e850df23f5aa19390f9e
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 17:46:44 +1100

    removed old titles

--- client/src/utils/list-titles.js
+++ client/src/utils/list-titles.js
@@ -1,12 +1,3 @@
-export const TITLE = "Title"
-export const CONTINUOUS = "Continuous";
-export const PREPARE = "Prepare";
-export const REPS = "Reps";
-export const WORK = "Work";
-export const REST = "Rest";
-export const SETS = "Sets";
-export const BREAK = "Break";
-
 export const TITLES = {
   continuous: "Continuous",
   prepare: "Prepare",
