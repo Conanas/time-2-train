29593d91360a3cf8784ebd04f33ba7ca04460d26
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 17:46:32 +1100

    removed unecessary code

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,25 +1,5 @@
 import React, { createContext, useReducer, useContext } from "react";
-import {
-  SET_WORKOUT,
-  SET_TITLE,
-  SET_CONTINUOUS,
-  SET_PREPARE,
-  SET_REPS,
-  SET_WORK,
-  SET_REST,
-  SET_SETS,
-  SET_BREAK
-} from './actions';
-// import {
-//   TITLE,
-//   CONTINUOUS,
-//   PREPARE,
-//   REPS,
-//   WORK,
-//   REST,
-//   SETS,
-//   BREAK
-// } from './list-titles';
+import { SET_ACTIONS } from './actions';
 
 const WorkoutContext = createContext();
 
@@ -34,122 +14,55 @@ const DEFAULT_STATE = {
   break: 180
 }
 
-// const DEFAULT_STATE = {
-//   title: {
-//     action: SET_TITLE,
-//     title: TITLE,
-//     value: "Test Title"
-//   },
-//   continuous: {
-//     action: SET_CONTINUOUS,
-//     title: CONTINUOUS,
-//     value: false
-//   },
-//   prepare: {
-//     action: SET_PREPARE,
-//     title: PREPARE,
-//     value: 15,
-//   },
-//   reps: {
-//     action: SET_REPS,
-//     title: REPS,
-//     value: 4,
-//   },
-//   work: {
-//     action: SET_WORK,
-//     title: WORK,
-//     value: 0,
-//   },
-//   rest: {
-//     action: SET_REST,
-//     title: REST,
-//     value: 30,
-//   },
-//   sets: {
-//     action: SET_SETS,
-//     title: SETS,
-//     value: 3,
-//   },
-//   break: {
-//     action: SET_BREAK,
-//     title: BREAK,
-//     value: 180,
-//   }
-// }
-
 const reducer = (state, action) => {
   switch (action.type) {
-    // case SET_WORKOUT:
-    //   return {
-    //     title: {
-    //       ...state.title,
-    //       value: action.payload.title
-    //     },
-    //     continuous: {
-    //       ...state.title,
-    //       value: action.payload.continuous
-    //     },
-    //     prepare: {
-    //       ...state.title,
-    //       value: action.payload.prepare
-    //     },
-    //     reps: {
-    //       ...state.title,
-    //       value: action.payload.reps
-    //     },
-    //     work: {
-    //       ...state.title,
-    //       value: action.payload.work
-    //     },
-    //     rest: {
-    //       ...state.title,
-    //       value: action.payload.rest
-    //     },
-    //     sets: {
-    //       ...state.title,
-    //       value: action.payload.sets
-    //     },
-    //     break: {
-    //       ...state.title,
-    //       value: action.payload.break
-    //     }
-    //   }
-    case SET_TITLE:
+    case SET_ACTIONS.workout:
+      return {
+        title: action.payload.title,
+        continuous: action.payload.continuous,
+        prepare: action.payload.prepare,
+        reps: action.payload.reps,
+        work: action.payload.work,
+        rest: action.payload.rest,
+        sets: action.payload.sets,
+        break: action.payload.break
+      }
+    case SET_ACTIONS.title:
       return {
         ...state,
         title: action.payload
       }
-    case SET_CONTINUOUS:
+    case SET_ACTIONS.continuous:
       return {
         ...state,
         continuous: action.payload
       }
-    case SET_PREPARE:
+    case SET_ACTIONS.prepare:
       return {
         ...state,
         prepare: state.prepare + action.payload
       }
-    case SET_REPS:
+    case SET_ACTIONS.reps:
       return {
         ...state,
         reps: state.reps + action.payload
       }
-    case SET_WORK:
+    case SET_ACTIONS.work:
       return {
         ...state,
         work: state.work + action.payload
       }
-    case SET_REST:
+    case SET_ACTIONS.rest:
       return {
         ...state,
         rest: state.rest + action.payload
       }
-    case SET_SETS:
+    case SET_ACTIONS.sets:
       return {
         ...state,
         sets: state.sets + action.payload
       }
-    case SET_BREAK:
+    case SET_ACTIONS.break:
       return {
         ...state,
         break: state.break + action.payload
@@ -158,112 +71,6 @@ const reducer = (state, action) => {
       return state;
   }
 }
-
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case SET_WORKOUT:
-//       return {
-//         title: {
-//           ...state.title,
-//           value: action.payload.title
-//         },
-//         continuous: {
-//           ...state.title,
-//           value: action.payload.continuous
-//         },
-//         prepare: {
-//           ...state.title,
-//           value: action.payload.prepare
-//         },
-//         reps: {
-//           ...state.title,
-//           value: action.payload.reps
-//         },
-//         work: {
-//           ...state.title,
-//           value: action.payload.work
-//         },
-//         rest: {
-//           ...state.title,
-//           value: action.payload.rest
-//         },
-//         sets: {
-//           ...state.title,
-//           value: action.payload.sets
-//         },
-//         break: {
-//           ...state.title,
-//           value: action.payload.break
-//         }
-//       }
-//     case SET_TITLE:
-//       return {
-//         ...state,
-//         title: {
-//           ...state.title,
-//           value: action.payload
-//         }
-//       }
-//     case SET_CONTINUOUS:
-//       return {
-//         ...state,
-//         continuous: {
-//           ...state.continuous,
-//           value: action.payload
-//         }
-//       }
-//     case SET_PREPARE:
-//       return {
-//         ...state,
-//         prepare: {
-//           ...state.prepare,
-//           value: state.prepare.value + action.payload
-//         }
-//       }
-//     case SET_REPS:
-//       return {
-//         ...state,
-//         reps: {
-//           ...state.reps,
-//           value: state.reps.value + action.payload
-//         }
-//       }
-//     case SET_WORK:
-//       return {
-//         ...state,
-//         work: {
-//           ...state.work,
-//           value: state.work.value + action.payload
-//         }
-//       }
-//     case SET_REST:
-//       return {
-//         ...state,
-//         rest: {
-//           ...state.rest,
-//           value: state.rest.value + action.payload
-//         }
-//       }
-//     case SET_SETS:
-//       return {
-//         ...state,
-//         sets: {
-//           ...state.sets,
-//           value: state.sets.value + action.payload
-//         }
-//       }
-//     case SET_BREAK:
-//       return {
-//         ...state,
-//         break: {
-//           ...state.break,
-//           value: state.break.value + action.payload
-//         }
-//       }
-//     default:
-//       return state;
-//   }
-// }
 
 const WorkoutProvider = (props) => {
   const [state, dispatch] = useReducer(reducer, DEFAULT_STATE);
