0168d5804caf342b9d092422ebf23477b7915030
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 20:53:38 +1100

    added create workout function

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -12,5 +12,11 @@ module.exports = {
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Workout
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
 }
