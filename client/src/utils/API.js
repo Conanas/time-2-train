bfad058a26202ba61e42bace498efd866d22c70c
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 16:00:51 +1100

    added get all workouts function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export default {
+  getWorkouts: function () {
+    return axios('/api/workouts');
+  },
   getWorkout: function (id) {
-    return axios.get(`/api/workout/:${id}`)
+    return axios.get(`/api/workouts/${id}`)
   }
 };
