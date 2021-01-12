50586b6af7d8b05bee08d2f7ee766c022fec4d77
Tom Haigh <tmhaigh@gmail.com>
2021-01-12 12:40:48 +1100

    put navbar into container

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
+import './style.css';
 
 export default function Navbar() {
   return (
     <>
       <nav>
-        <div class="nav-wrapper">
-          <a href="#!" class="brand-logo">Logo</a>
+        <div class="nav-wrapper container">
+          <a href="#!" class="brand-logo right">Workout Time</a>
           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
+          <ul class="left hide-on-med-and-down">
+            <li><a className="nav-item" href="/">Start/Edit Timer</a></li>
           </ul>
         </div>
       </nav>
 
       <ul class="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+        <li><a href="/">Start/Edit Timer</a></li>
       </ul>
     </>
   )
