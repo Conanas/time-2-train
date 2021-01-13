59f9dc1917035ec68a3671969a8090c0c9280d5d
Tom Haigh <tmhaigh@gmail.com>
2021-01-13 12:58:40 +1100

    changed sidenav button back to anchor

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,8 +6,8 @@ export default function Navbar() {
     <>
       <nav>
         <div className="nav-wrapper container">
-          <a href="#!" className="brand-logo right">Climb Time</a>
-          <button data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></button>
+          <a href="#!" className="brand-logo right">Workout Time</a>
+          <a data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <ul className="left hide-on-med-and-down">
             <li><a className="nav-item" href="/">Start/Edit Timer</a></li>
             <li><a className="nav-item" href="/load">Load Workout</a></li>
