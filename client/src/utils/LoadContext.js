3ead0a7edde762bdf217bf9fae1ee74a8cb2c164
Tom Haigh <tmhaigh@gmail.com>
2021-01-13 12:33:40 +1100

    set default state to empty array

--- client/src/utils/LoadContext.js
+++ client/src/utils/LoadContext.js
@@ -1,14 +1,16 @@
 import React, { createContext, useReducer, useContext } from "react";
+import { SET_ACTIONS } from "./actions";
 
 const LoadContext = createContext();
 
-const DEFAULT_STATE = {
-
-}
+const DEFAULT_STATE = []
 
 const reducer = (state, action) => {
   switch (action.type) {
-
+    case SET_ACTIONS.import:
+      return action.payload
+    default:
+      return state;
   }
 }
 
