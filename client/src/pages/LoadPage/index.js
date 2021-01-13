bbd644d86d65dc2114b475ef0c14c5f961fd6e3a
Tom Haigh <tmhaigh@gmail.com>
2021-01-13 12:19:14 +1100

    added useLoadContext and useWorkoutContext

--- client/src/pages/LoadPage/index.js
+++ client/src/pages/LoadPage/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useWorkoutContext } from '../../utils/GlobalState';
+import { useLoadContext } from '../../utils/LoadContext';
+import API from '../../utils/API';
 import './style.css';
 
 export default function LoadPage() {
+  const [loadState, loadDispatch] = useLoadContext();
+  const [workoutState, workoutDispatch] = useWorkoutContext();
+
+  useEffect(() => {
+    API.getWorkouts()
+      .then(res => {
+        loadDispatch({ type: '', payload: '' })
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <>
       <h1 className="flow-text">Load Workout</h1>
       <ul>
-
+        {Object.keys(state).map((workout, index) => {
+          return (
+            <li key={index}>{workout.title}</li>
+          )
+        })}
       </ul>
     </>
   )
