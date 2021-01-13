04b63d38e8fe6eca9a6a09e6adc71685451fa23a
Tom Haigh <tmhaigh@gmail.com>
2021-01-13 12:33:30 +1100

    added SET_ACTIONS.import

--- client/src/pages/LoadPage/index.js
+++ client/src/pages/LoadPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useWorkoutContext } from '../../utils/GlobalState';
 import { useLoadContext } from '../../utils/LoadContext';
+import { SET_ACTIONS } from '../../utils/actions';
 import API from '../../utils/API';
 import './style.css';
 
@@ -11,16 +12,17 @@ export default function LoadPage() {
   useEffect(() => {
     API.getWorkouts()
       .then(res => {
-        loadDispatch({ type: '', payload: '' })
+        loadDispatch({ type: SET_ACTIONS.import, payload: res.data })
       })
       .catch(error => console.log(error))
   }, [])
 
   return (
     <>
+      {console.log(loadState)}
       <h1 className="flow-text">Load Workout</h1>
       <ul>
-        {Object.keys(state).map((workout, index) => {
+        {loadState.map((workout, index) => {
           return (
             <li key={index}>{workout.title}</li>
           )
