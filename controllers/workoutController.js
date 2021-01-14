5ffaea769b944af21ab7e572ec5a7f4ac8efcdd2
Tom Haigh <tmhaigh@gmail.com>
2021-01-14 17:42:28 +1100

    created update function to db

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -18,5 +18,11 @@ module.exports = {
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Workout
+      .updateOne({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
 }
