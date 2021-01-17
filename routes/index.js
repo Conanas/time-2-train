ca4fbad2722cb6ef422876f869758a8e81148e4e
Tom Haigh <tmhaigh@gmail.com>
2021-01-18 09:38:38 +1100

    added routes to auth index

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const authRoutes = require("./auth");
 
+// Auth Routes
+router.use("/auth", authRoutes);
+
+// router.get('/auth/google', (req, res) => {
+//   res.send('Logging in with Google')
+// })
+
 // API Routes
 router.use("/api", apiRoutes);
-
-// auth Routes
-router.use("/auth", authRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
