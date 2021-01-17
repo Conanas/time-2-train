279630a23afee4b1432492313361eb224b771d41
Tom Haigh <tmhaigh@gmail.com>
2021-01-17 15:21:08 +1100

    created intial loginpage jsx

---
+++ client/src/pages/LoginPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function LoginPage() {
+  return (
+    <>
+      <h4>Login</h4>
+      <button>Google+</button>
+    </>
+  )
+}
