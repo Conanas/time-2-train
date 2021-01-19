ea44669ceaf5270157af94a7e5db3fb03ae785b0
Tom Haigh <tmhaigh@gmail.com>
2021-01-20 08:56:53 +1100

    moved get user to :id path

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
+// /users routes
 router
   .route('/')
-  .post(userController.create);
+  .post(userController.create)
 
 router
   .route("/:id")
-  .get(userController.findOne)
+  .get(userController.findOneByEmail);
 
 module.exports = router;
