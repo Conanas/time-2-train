8f8ae75e3d9bbac5bbccab57641e73469b447276
Tom Haigh <tmhaigh@gmail.com>
2021-01-20 10:03:30 +1100

    removed passport

--- server.js
+++ server.js
@@ -2,19 +2,10 @@ require('dotenv').config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require('cors');
-const passport = require('passport');
 const mongoose = require("mongoose");
 const routes = require("./routes/");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const passportSetup = require('./config/passport-setup');
-
-// initalize passport
-app.use(session({
-  secret: 'coding',
-}))
-app.use(passport.initialize());
-app.use(passport.session());
 
 // set up cors to allow us to accept requests from our client
 app.use(
@@ -42,21 +33,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true
 });
 
-const authCheck = (req, res, next) => {
-  console.log("req user", req.user)
-  if (!req.user) {
-    res.status(401).json({
-      authenticated: false,
-      message: "user has not been authenticated"
-    });
-  } else {
-    next();
-  }
-};
-
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("/", authCheck, function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
