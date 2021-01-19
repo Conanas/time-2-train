da6b3dd1b5783859545c5922e2d980f84e992f35
Tom Haigh <tmhaigh@gmail.com>
2021-01-20 09:16:25 +1100

    added dispatchUser on logout

--- client/src/components/GoogleButtons/GoogleLogout.js
+++ client/src/components/GoogleButtons/GoogleLogout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useGoogleLogout } from 'react-google-login';
+import { useUserContext } from '../../utils/UserContext';
+import { SET_ACTIONS } from '../../utils/actions';
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 export default function GoogleLogout() {
+  const [userState, dispatchUser] = useUserContext();
 
   const onLogoutSuccess = (res) => {
+    dispatchUser({ type: SET_ACTIONS.userLogout })
     console.log('Logged out Success');
-    alert('Logged out Successfully ✌');
   };
 
   const onFailure = () => {
