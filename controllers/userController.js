2e4c2a5ebf5382da524c73b92e756ac3cc439be7
Tom Haigh <tmhaigh@gmail.com>
2021-01-20 08:56:26 +1100

    removed redundant findById and findOne funcs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,9 @@
 const db = require("../models");
 
 module.exports = {
-  findById: function (req, res) {
+  findOneByEmail: function (req, res) {
     db.User
-      .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  findOne: function (req, res) {
-    db.User
-      .findOne({ googleId: req.params.id })
+      .findOne({ email: req.params.id })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
