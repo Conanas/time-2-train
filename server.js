d8073cf452221d639a5fa9f501e37908186f1645
Tom Haigh <tmhaigh@gmail.com>
2021-01-20 10:04:08 +1100

    removed redundant code

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 require('dotenv').config();
 const express = require("express");
 const logger = require("morgan");
-const cors = require('cors');
 const mongoose = require("mongoose");
 const routes = require("./routes/");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// set up cors to allow us to accept requests from our client
-app.use(
-  cors({
-    origin: "http://localhost:3000", // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true // allow session cookie from browser to pass through
-  })
-);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -35,9 +25,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
