176529dab818bc5f0aa10a1ab84c4482d998522d
Tom Haigh <tmhaigh@gmail.com>
2021-01-19 20:58:15 +1100

    created userController functions

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const db = require("../models");
+
+module.exports = {
+  findById: function (req, res) {
+    db.User
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findOne: function (req, res) {
+    db.User
+      .findOne({ googleId: req.params.id })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.User
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+}
