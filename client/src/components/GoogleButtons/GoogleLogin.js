3883b568eedffd1a470dce9ecf61cc2009a2a2a6
Tom Haigh <tmhaigh@gmail.com>
2021-01-19 20:56:58 +1100

    created findOrCreateUser method

--- client/src/components/GoogleButtons/GoogleLogin.js
+++ client/src/components/GoogleButtons/GoogleLogin.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { useGoogleLogin } from 'react-google-login';
 import { refreshTokenSetup } from '../../utils/refreshToken';
+import API from '../../utils/API';
 import './style.css';
+import { get } from 'mongoose';
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 export default function GoogleLogin() {
 
+  const getOrCreateUser = async (profileObj) => {
+    try {
+      console.log(profileObj.googleId)
+      let user = await API.getUser(profileObj.googleId)
+      if (!user.data) {
+        console.log("user: ", user)
+        let createdUser = await API.createUser(profileObj)
+        console.log("createdUser: ", createdUser)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const onSuccess = (res) => {
     console.log('Login Success: currentUser:', res.profileObj);
-    alert(
-      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
-    );
+    getOrCreateUser(res.profileObj)
     refreshTokenSetup(res);
   };
 
   const onFailure = (res) => {
     console.log('Login failed: res:', res);
-    alert(
-      `Failed to login. 😢 Please ping this to repo owner twitter.com/sivanesh_fiz`
-    );
+    alert("Login failed");
   };
 
   const { signIn } = useGoogleLogin({
