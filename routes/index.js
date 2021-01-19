09c8842cbde150ccc554865f6c1d24d319d6d79e
Tom Haigh <tmhaigh@gmail.com>
2021-01-20 10:05:29 +1100

    removed auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const authRoutes = require("./auth");
-
-// Auth Routes
-router.use("/auth", authRoutes);
 
 // API Routes
 router.use("/api", apiRoutes);
