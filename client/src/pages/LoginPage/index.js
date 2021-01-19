439a0b074fe0e16d7c7079798f742a6530f81418
Tom Haigh <tmhaigh@gmail.com>
2021-01-19 11:44:22 +1100

    initial login page setup

---
+++ client/src/pages/LoginPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoogleLogin from '../../components/LoginButtons/GoogleLogin/';
+import './style.css';
+
+export default function LoginPage() {
+  return (
+    <>
+      <h4>Login</h4>
+      <GoogleLogin />
+    </>
+  )
+}
