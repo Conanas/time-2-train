552ed12fa3657af7e1996f0342a21ce0c7859c1a
Tom Haigh <tmhaigh@gmail.com>
2021-01-19 12:05:01 +1100

    moved to GoogleButton directory

---
+++ client/src/components/GoogleButtons/GoogleLogout.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useGoogleLogout } from 'react-google-login';
+import './style.css';
+
+const clientId = '21174679375-ueqsp2fchfubf00bq445tkki7voqs47c.apps.googleusercontent.com';
+
+export default function GoogleLogout() {
+
+  const onLogoutSuccess = (res) => {
+    console.log('Logged out Success');
+    alert('Logged out Successfully ✌');
+  };
+
+  const onFailure = () => {
+    console.log('Handle failure cases');
+  };
+
+  const { signOut } = useGoogleLogout({
+    clientId,
+    onLogoutSuccess,
+    onFailure,
+  });
+
+  return (
+    <button onClick={signOut} className="button">
+      <img src="/icons/google.svg" alt="google login" className="icon"></img>
+
+      <span className="buttonText">Sign out</span>
+    </button>
+  );
+}
