636f788febe2be7c264b67fa5b9b2b35835d73aa
Tom Haigh <tmhaigh@gmail.com>
2021-01-21 19:02:29 +1100

    added userstate, loads workouts if signed in

--- client/src/pages/LoadPage/index.js
+++ client/src/pages/LoadPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useWorkoutContext } from '../../utils/WorkoutContext';
 import { useLoadContext } from '../../utils/LoadContext';
+import { userUserContext, useUserContext } from '../../utils/UserContext';
 import { SET_ACTIONS } from '../../utils/actions';
 import API from '../../utils/API';
 import LoadModal from '../../components/LoadModal/';
@@ -9,6 +10,7 @@ import './style.css';
 export default function LoadPage() {
   const [loadState, loadDispatch] = useLoadContext();
   const [, workoutDispatch] = useWorkoutContext();
+  const [userState, dispatchUser] = useUserContext();
 
   useEffect(() => {
     API.getWorkouts()
@@ -22,23 +24,30 @@ export default function LoadPage() {
     <>
       <h4>Load Workout</h4>
       <div className="load-list">
-        {loadState.map((workout, index) => {
-          return (
-            <label className="load-label flow-text" key={index}>
-              <div>
-                <input className="load-radio" type="radio" name="workouts" id={workout._id} onChange={(() => workoutDispatch({ type: SET_ACTIONS.workout, payload: workout }))} />
-                <span><label className="flow-text">{workout.title}</label></span>
-              </div>
-              <div>
-                <i className="fas fa-times"></i>
-              </div>
-            </label>
-          )
-        })}
+        {userState.email === null ?
+          <label className="flow-text">You must be signed in to load a workout</label>
+          :
+          loadState.map((workout, index) => {
+            return (
+              <label className="load-label flow-text" key={index}>
+                <div>
+                  <input className="load-radio" type="radio" name="workouts" id={workout._id} onChange={(() => workoutDispatch({ type: SET_ACTIONS.workout, payload: workout }))} />
+                  <span><label className="flow-text">{workout.title}</label></span>
+                </div>
+                <div>
+                  <i className="fas fa-times"></i>
+                </div>
+              </label>
+            )
+          })
+        }
       </div>
-      <div className="button-div">
-        <button className="show-button modal-trigger" data-target="load-modal">Show</button>
-      </div>
+      {userState.email === null ?
+        null
+        :
+        <div className="button-div">
+          <button className="show-button modal-trigger" data-target="load-modal">Show</button>
+        </div>}
       <LoadModal />
     </>
   )
