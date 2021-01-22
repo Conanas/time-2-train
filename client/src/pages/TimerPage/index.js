d4f03754d009e0787d7f01dbbca0a3fe58e19941
Tom Haigh <tmhaigh@gmail.com>
2021-01-22 17:49:13 +1100

    added workoutContext and useEffect func

--- client/src/pages/TimerPage/index.js
+++ client/src/pages/TimerPage/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useWorkoutContext } from '../../utils/WorkoutContext';
 import './style.css';
 
 export default function TimerPage() {
+  const [workoutState, dispatchWorkout] = useWorkoutContext();
+  console.log(workoutState)
+
+  useEffect(() => {
+    console.log(workoutState)
+  }, [])
+
   return (
     <>
+      {Object.keys(workoutState).map((key, index) => <label key={index}>{key} : {workoutState[key]}</label>)}
     </>
   )
 }
