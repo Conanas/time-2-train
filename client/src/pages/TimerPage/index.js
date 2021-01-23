f2d8d4614a997d51469b99763e8f68cfe60fbe11
Tom Haigh <tmhaigh@gmail.com>
2021-01-23 20:48:22 +1100

    now displays the workout elements

--- client/src/pages/TimerPage/index.js
+++ client/src/pages/TimerPage/index.js
@@ -1,18 +1,34 @@
 import React, { useEffect } from 'react';
+import API from '../../utils/API';
+import { SET_ACTIONS } from '../../utils/actions';
 import { useWorkoutContext } from '../../utils/WorkoutContext';
 import './style.css';
 
 export default function TimerPage() {
   const [workoutState, dispatchWorkout] = useWorkoutContext();
-  console.log(workoutState)
 
   useEffect(() => {
     console.log(workoutState)
+    let workoutId = localStorage.workoutId
+    API.getWorkout(workoutId)
+      .then(res => {
+        if (res.data) {
+          dispatchWorkout({ type: SET_ACTIONS.workout, payload: res.data });
+        }
+      }).catch(err => console.log(err))
   }, [])
 
   return (
     <>
-      {Object.keys(workoutState).map((key, index) => <label key={index}>{key} : {workoutState[key]}</label>)}
+      {Object.keys(workoutState).map((key, index) => {
+        if (key === "_id") {
+          return null
+        }
+        if (key === "continuous") {
+          return <label key={index}>{key} : {workoutState[key] === true ? "true" : "false"}</label>
+        }
+        return <label key={index}>{key} : {workoutState[key]}</label>
+      })}
     </>
   )
 }
