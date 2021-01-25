a5be354726e0b34ebb863916df97bff0531d8241
Tom Haigh <tmhaigh@gmail.com>
2021-01-25 21:18:01 +1100

    added must be signed in to load workouts

--- client/src/pages/LoadPage/index.js
+++ client/src/pages/LoadPage/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { useLoadContext } from '../../utils/LoadContext';
 import { useUserContext } from '../../utils/UserContext';
-import { SET_ACTIONS } from '../../utils/actions';
+import { MESSAGES, SET_ACTIONS } from '../../utils/actions';
 import API from '../../utils/API';
 import LoadModal from '../../components/Modals/LoadModal/';
 import DeleteModal from '../../components/Modals/DeleteModal/';
@@ -46,28 +46,32 @@ export default function LoadPage() {
     <>
       <h4>Load Workout</h4>
       <div className="load-list">
-        <ul
-          ref={collapsibleRef}
-          className="collapsible popout"
-        >
-          {loadState.map((workout, index) => {
-            return (
-              <li key={index} onClick={() => setSelect(workout)}>
-                <div className="collapsible-header">
-                  {workout.title}
-                </div>
-                <div className="collapsible-body flow-text">
-                  <div className="collapsible-body-div">
-                    <button className="modal-trigger" data-target="load-modal"><i className="fas fa-play"></i></button>
-                    <button><i className="fas fa-edit"></i></button>
-                    <button className="modal-trigger" data-target="delete-modal"><i className="fas fa-trash-alt"></i></button>
+        {userState._id === null ?
+          <label className="flow-text">{MESSAGES.MUST_BE_SIGNED_IN_TO_LOAD}</label>
+          :
+          <ul
+            ref={collapsibleRef}
+            className="collapsible popout"
+          >
+            {loadState.map((workout, index) => {
+              return (
+                <li key={index} onClick={() => setSelect(workout)}>
+                  <div className="collapsible-header">
+                    {workout.title}
                   </div>
-                </div>
-              </li>
-            )
-          })
-          }
-        </ul>
+                  <div className="collapsible-body flow-text">
+                    <div className="collapsible-body-div">
+                      <button className="modal-trigger" data-target="load-modal"><i className="fas fa-play"></i></button>
+                      <button><i className="fas fa-edit"></i></button>
+                      <button className="modal-trigger" data-target="delete-modal"><i className="fas fa-trash-alt"></i></button>
+                    </div>
+                  </div>
+                </li>
+              )
+            })
+            }
+          </ul>
+        }
       </div>
       <LoadModal workout={selectState ? selectState : null} />
       <DeleteModal getWorkouts={getWorkouts} deleteWorkout={deleteWorkout} workoutId={selectState ? selectState._id : null} />
