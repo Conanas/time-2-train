a50d3290505aa3581cb33c26f8c6d46473d39884
Tom Haigh <tmhaigh@gmail.com>
2021-01-25 21:17:45 +1100

    removed start edit workout from nav

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -13,7 +13,6 @@ export default function Navbar() {
           <Link to="/" className="brand-logo right">Workout Time</Link>
           <a data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <ul className="left hide-on-med-and-down">
-            <li><Link className="nav-item" to="/">Start/Edit Workout</Link></li>
             <li><Link className="nav-item" to="/create">Create Workout</Link></li>
             <li><Link className="nav-item" to="/load">Load Workout</Link></li>
           </ul>
@@ -21,7 +20,6 @@ export default function Navbar() {
       </nav>
 
       <ul className="sidenav" id="mobile-demo">
-        <li><Link to="/">Start/Edit Workout</Link></li>
         <li><Link to="/create">Create Workout</Link></li>
         <li><Link to="/load">Load Workout</Link></li>
       </ul>
