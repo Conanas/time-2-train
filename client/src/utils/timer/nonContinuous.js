0c96f3ad72aa3f2ee81f1b3788658fe2200f4259
Tom Haigh <tmhaigh@gmail.com>
2021-01-28 12:11:25 +1100

    background color only changes in break

--- client/src/utils/timer/nonContinuous.js
+++ client/src/utils/timer/nonContinuous.js
@@ -1,25 +1,16 @@
 module.exports = {
   startTimerNonContinuous: (timerState, setTimerState, workoutState, MODES, backgroundColor, BACKGROUND_COLORS) => {
-    if (timerState.mode === MODES.PREPARE) {
-      backgroundColor = BACKGROUND_COLORS.PREPARE;
-      document.body.style.backgroundColor = backgroundColor;
-    }
-
     if (timerState.mode === MODES.WORK) {
       if (timerState.rep === workoutState.reps) {
         backgroundColor = BACKGROUND_COLORS.BREAK;
         document.body.style.backgroundColor = backgroundColor;
         setTimerState({ mode: MODES.BREAK, countdown: workoutState.break, rep: timerState.rep, set: timerState.set })
       } else {
-        backgroundColor = BACKGROUND_COLORS.REST;
-        document.body.style.backgroundColor = backgroundColor;
         setTimerState({ mode: MODES.REST, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
       }
     }
 
     if (timerState.mode === MODES.REST) {
-      backgroundColor = BACKGROUND_COLORS.WORK;
-      document.body.style.backgroundColor = backgroundColor;
       setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep + 1, set: timerState.set })
     }
   },
@@ -31,8 +22,6 @@ module.exports = {
         document.body.style.backgroundColor = backgroundColor;
         setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep, set: timerState.set })
       } else {
-        backgroundColor = BACKGROUND_COLORS.WORK;
-        document.body.style.backgroundColor = backgroundColor;
         setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
       }
     }
@@ -43,14 +32,12 @@ module.exports = {
         document.body.style.backgroundColor = backgroundColor;
         setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep + 1, set: timerState.set })
       } else {
-        backgroundColor = BACKGROUND_COLORS.WORK;
-        document.body.style.backgroundColor = backgroundColor;
         setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep + 1, set: timerState.set })
       }
     }
 
     if (timerState.mode === MODES.BREAK) {
-      backgroundColor = BACKGROUND_COLORS.WORK;
+      backgroundColor = BACKGROUND_COLORS.INITIAL;
       document.body.style.backgroundColor = backgroundColor;
       setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: 1, set: timerState.set + 1 })
     }
