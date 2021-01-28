e390a2380998b337b37d5d8cc23c32d25622007f
Tom Haigh <tmhaigh@gmail.com>
2021-01-29 10:13:37 +1100

    added homepage to navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -10,6 +10,7 @@ export default function Navbar() {
           <Link to="/" className="brand-logo right">Time-2-Train</Link>
           <a data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <ul className="left hide-on-med-and-down">
+            <li><Link className="nav-item" to="/">Home</Link></li>
             <li><Link className="nav-item" to="/create">Create Workout</Link></li>
             <li><Link className="nav-item" to="/load">Load Workout</Link></li>
           </ul>
@@ -17,6 +18,7 @@ export default function Navbar() {
       </nav>
 
       <ul className="sidenav" id="mobile-demo">
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/create">Create Workout</Link></li>
         <li><Link to="/load">Load Workout</Link></li>
       </ul>
--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './style.css';
 
 export default function HomePage() {
+
+  useEffect(() => {
+    let sidenav = document.querySelector('#mobile-demo');
+    M.Sidenav.init(sidenav, {});
+  }, [])
+
   return (
     <>
       <h4>Welcome to Time-2-Train</h4>
