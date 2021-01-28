039dd1d75d0ac15638d499b6aeb16b994811d86e
Tom Haigh <tmhaigh@gmail.com>
2021-01-28 12:04:15 +1100

    moved onStart and onComplete to separate files

--- client/src/pages/TimerPage/index.js
+++ client/src/pages/TimerPage/index.js
@@ -4,6 +4,8 @@ import Countdown, { zeroPad } from 'react-countdown';
 import useSound from 'use-sound';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { useWorkoutContext } from '../../utils/WorkoutContext';
+import { startTimerContinuous, onCompleteContinuous } from '../../utils/timer/continuous';
+import { startTimerNonContinuous, onCompleteNonContinuous } from '../../utils/timer/nonContinuous';
 import './style.css';
 
 import beep2 from '../../assets/beep-1.wav';
@@ -51,46 +53,15 @@ export default function TimerPage() {
     return () => document.body.style.backgroundColor = BACKGROUND_COLORS.INITIAL;
   }, [])
 
+  function changeBackground(colour) {
+
+  }
+
   function startTimer() {
     if (workoutState.continuous === false) {
-
-      if (timerState.mode === MODES.PREPARE) {
-        backgroundColor = BACKGROUND_COLORS.PREPARE;
-        document.body.style.backgroundColor = backgroundColor;
-      }
-
-      if (timerState.mode === MODES.WORK) {
-        if (timerState.rep === workoutState.reps) {
-          backgroundColor = BACKGROUND_COLORS.BREAK;
-          document.body.style.backgroundColor = backgroundColor;
-          setTimerState({ mode: MODES.BREAK, countdown: workoutState.break, rep: timerState.rep, set: timerState.set })
-        } else {
-          backgroundColor = BACKGROUND_COLORS.REST;
-          document.body.style.backgroundColor = backgroundColor;
-          setTimerState({ mode: MODES.REST, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
-        }
-      }
-
-      if (timerState.mode === MODES.REST) {
-        backgroundColor = BACKGROUND_COLORS.WORK;
-        document.body.style.backgroundColor = backgroundColor;
-        setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep + 1, set: timerState.set })
-      }
-
+      startTimerNonContinuous(timerState, setTimerState, workoutState, MODES, backgroundColor, BACKGROUND_COLORS);
     } else {
-
-      if (timerState.mode === MODES.WORK) {
-        if (timerState.rep === workoutState.reps) {
-          setTimerState({ mode: MODES.BREAK, countdown: workoutState.break, rep: timerState.rep, set: timerState.set })
-        } else {
-          setTimerState({ mode: MODES.REST, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
-        }
-      }
-
-      if (timerState.mode === MODES.REST) {
-        setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep + 1, set: timerState.set })
-      }
-
+      startTimerContinuous(timerState, setTimerState, workoutState, MODES, backgroundColor, BACKGROUND_COLORS);
     }
     timerRef.current.api.start();
     setPlayState(true)
@@ -104,56 +75,10 @@ export default function TimerPage() {
     play();
     setPlayState(false)
     if (workoutState.continuous === false) {
-
-      if (timerState.mode === MODES.PREPARE) {
-        if (workoutState.reps === 1 && workoutState.sets === 1) {
-          backgroundColor = BACKGROUND_COLORS.COMPLETED;
-          document.body.style.backgroundColor = backgroundColor;
-          setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep, set: timerState.set })
-        } else {
-          backgroundColor = BACKGROUND_COLORS.WORK;
-          document.body.style.backgroundColor = backgroundColor;
-          setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
-        }
-      }
-
-      if (timerState.mode === MODES.REST) {
-        if (timerState.rep === workoutState.reps - 1 && timerState.set === workoutState.sets) {
-          backgroundColor = BACKGROUND_COLORS.COMPLETED;
-          document.body.style.backgroundColor = backgroundColor;
-          setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep + 1, set: timerState.set })
-        } else {
-          backgroundColor = BACKGROUND_COLORS.WORK;
-          document.body.style.backgroundColor = backgroundColor;
-          setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep + 1, set: timerState.set })
-        }
-      }
-
-      if (timerState.mode === MODES.BREAK) {
-        backgroundColor = BACKGROUND_COLORS.WORK;
-        document.body.style.backgroundColor = backgroundColor;
-        setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: 1, set: timerState.set + 1 })
-      }
-
+      onCompleteNonContinuous(timerState, setTimerState, workoutState, MODES, backgroundColor, BACKGROUND_COLORS);
     } else {
-
-      if (timerState.mode === MODES.PREPARE) {
-        setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep, set: timerState.set })
-      }
-
-      if (timerState.mode === MODES.REST) {
-        if (timerState.rep === workoutState.reps - 1 && timerState.set === workoutState.sets) {
-          setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep + 1, set: timerState.set })
-        } else {
-          setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep + 1, set: timerState.set })
-        }
-      }
-
-      if (timerState.mode === MODES.BREAK) {
-        setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: 1, set: timerState.set + 1 })
-      }
+      onCompleteContinuous(timerState, setTimerState, workoutState, MODES, backgroundColor, BACKGROUND_COLORS);
       startTimer();
-
     }
   }
 
---
+++ client/src/utils/timer/continuous.js
@@ -0,0 +1,52 @@
+module.exports = {
+  startTimerContinuous: (timerState, setTimerState, workoutState, MODES, backgroundColor, BACKGROUND_COLORS) => {
+    if (timerState.mode === MODES.PREPARE) {
+      backgroundColor = BACKGROUND_COLORS.PREPARE;
+      document.body.style.backgroundColor = backgroundColor;
+    }
+
+    if (timerState.mode === MODES.WORK) {
+      if (timerState.rep === workoutState.reps) {
+        backgroundColor = BACKGROUND_COLORS.BREAK;
+        document.body.style.backgroundColor = backgroundColor;
+        setTimerState({ mode: MODES.BREAK, countdown: workoutState.break, rep: timerState.rep, set: timerState.set })
+      } else {
+        backgroundColor = BACKGROUND_COLORS.REST;
+        document.body.style.backgroundColor = backgroundColor;
+        setTimerState({ mode: MODES.REST, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
+      }
+    }
+
+    if (timerState.mode === MODES.REST) {
+      backgroundColor = BACKGROUND_COLORS.WORK;
+      document.body.style.backgroundColor = backgroundColor;
+      setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep + 1, set: timerState.set })
+    }
+  },
+
+  onCompleteContinuous: (timerState, setTimerState, workoutState, MODES, backgroundColor, BACKGROUND_COLORS) => {
+    if (timerState.mode === MODES.PREPARE) {
+      backgroundColor = BACKGROUND_COLORS.WORK;
+      document.body.style.backgroundColor = backgroundColor;
+      setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep, set: timerState.set })
+    }
+
+    if (timerState.mode === MODES.REST) {
+      if (timerState.rep === workoutState.reps - 1 && timerState.set === workoutState.sets) {
+        backgroundColor = BACKGROUND_COLORS.COMPLETED;
+        document.body.style.backgroundColor = backgroundColor;
+        setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep + 1, set: timerState.set })
+      } else {
+        backgroundColor = BACKGROUND_COLORS.WORK;
+        document.body.style.backgroundColor = backgroundColor;
+        setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep + 1, set: timerState.set })
+      }
+    }
+
+    if (timerState.mode === MODES.BREAK) {
+      backgroundColor = BACKGROUND_COLORS.WORK;
+      document.body.style.backgroundColor = backgroundColor;
+      setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: 1, set: timerState.set + 1 })
+    }
+  }
+}
