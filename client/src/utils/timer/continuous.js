04df4bade6eea3176b93def3866d36624c9e54eb
Tom Haigh <tmhaigh@gmail.com>
2021-01-28 19:43:45 +1100

    added console. logs for testing

--- client/src/utils/timer/continuous.js
+++ client/src/utils/timer/continuous.js
@@ -2,28 +2,33 @@ module.exports = {
   startTimerContinuous: (timerState, setTimerState, workoutState, MODES, BACKGROUND_COLORS, setBackground, timerRef) => {
     if (timerState.mode === MODES.PREPARE) {
       // set background color for prepare mode
+      console.log(1)
       setBackground(BACKGROUND_COLORS.PREPARE)
     }
     else if (timerState.mode === MODES.WORK) {
       // in work mode
       if (timerState.rep === workoutState.reps && timerState.set === workoutState.sets) {
         // if on last rep of last set then finish
+        console.log(2)
         setBackground(BACKGROUND_COLORS.COMPLETED)
         setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep, set: timerState.set })
         timerRef.current.api.stop();
       }
       else if (timerState.rep === workoutState.reps) {
         // if on last rep of set go to break
+        console.log(3)
         setBackground(BACKGROUND_COLORS.BREAK)
         setTimerState({ mode: MODES.BREAK, countdown: workoutState.break, rep: timerState.rep, set: timerState.set })
       } else {
         // go to rest mode
+        console.log(4)
         setBackground(BACKGROUND_COLORS.REST)
         setTimerState({ mode: MODES.REST, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
       }
     }
     else if (timerState.mode === MODES.REST) {
       // if in rest mode go to work mode
+      console.log(5)
       setBackground(BACKGROUND_COLORS.WORK)
       setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep + 1, set: timerState.set })
     }
@@ -32,11 +37,13 @@ module.exports = {
   onCompleteContinuous: (timerState, setTimerState, workoutState, MODES, BACKGROUND_COLORS, setBackground) => {
     if (timerState.mode === MODES.PREPARE) {
       // go to work mode after prepare - first rep
+      console.log(6)
       setBackground(BACKGROUND_COLORS.WORK)
       setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: timerState.rep, set: timerState.set })
     }
     else if (timerState.mode === MODES.BREAK) {
       // go to work mode after break
+      console.log(7)
       setBackground(BACKGROUND_COLORS.WORK)
       setTimerState({ mode: MODES.WORK, countdown: workoutState.work, rep: 1, set: timerState.set + 1 })
     }
