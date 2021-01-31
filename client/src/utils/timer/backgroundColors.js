5198be823e225d7cee35539e09dcf0d255681080
Tom Haigh <tmhaigh@gmail.com>
2021-01-31 17:43:24 +1100

    changed label colors and break and rest colors

--- client/src/utils/timer/backgroundColors.js
+++ client/src/utils/timer/backgroundColors.js
@@ -2,7 +2,7 @@ export const BACKGROUND_COLORS = {
   DEFAULT: "#efeff5",
   PREPARE: "#efeff5",
   WORK: "#efeff5",
-  REST: "red",
-  BREAK: "blue",
+  REST: "#ed5e5e",
+  BREAK: "#668cff",
   COMPLETED: "#efeff5"
 }
