76b2cbf0bed36013fdb11cfd543a9d42b23ca9c6
Tom Haigh <tmhaigh@gmail.com>
2021-02-01 19:15:01 +1100

    removed console logs

--- client/src/utils/timer/nonContinuous.js
+++ client/src/utils/timer/nonContinuous.js
@@ -11,13 +11,11 @@ module.exports = {
       // after work
       if (timerState.rep === workoutState.reps) {
         // last rep of set go to break
-        console.log(1)
         BEEP_BREAK.play();
         setBackground(BACKGROUND_COLORS.BREAK)
         setTimerState({ mode: MODES.BREAK, countdown: workoutState.break, rep: timerState.rep, set: timerState.set })
       } else {
         // next rest
-        console.log(2)
         setBackground(BACKGROUND_COLORS.REST)
         setTimerState({ mode: MODES.REST, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
       }
@@ -29,13 +27,11 @@ module.exports = {
       // after prepare
       if (workoutState.reps === 1 && workoutState.sets === 1) {
         // last rep and set if there's only 1 rep and 1 set
-        console.log(3)
         BEEP_COMPLETED.play();
         setBackground(BACKGROUND_COLORS.COMPLETED)
         setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep, set: timerState.set })
       } else {
         // first rep after prepare
-        console.log(4)
         BEEP_GO.play();
         setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep, set: timerState.set })
       }
@@ -44,13 +40,11 @@ module.exports = {
       // after rest
       if (timerState.rep === workoutState.reps - 1 && timerState.set === workoutState.sets) {
         // last rep and set
-        console.log(5)
         BEEP_COMPLETED.play()
         setBackground(BACKGROUND_COLORS.COMPLETED)
         setTimerState({ mode: MODES.COMPLETED, countdown: 0, rep: timerState.rep + 1, set: timerState.set })
       } else {
         // next rep - work mode
-        console.log(6)
         BEEP_GO.play();
         setBackground(BACKGROUND_COLORS.WORK)
         setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: timerState.rep + 1, set: timerState.set })
@@ -58,7 +52,6 @@ module.exports = {
     }
     else if (timerState.mode === MODES.BREAK) {
       // after break time
-      console.log(7)
       BEEP_GO.play();
       setBackground(BACKGROUND_COLORS.DEFAULT)
       setTimerState({ mode: MODES.WORK, countdown: workoutState.rest, rep: 1, set: timerState.set + 1 })
