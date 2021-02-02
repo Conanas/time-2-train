c36036195a5539bbd0cbe3bea48ea78580a0fb53
Tom Haigh <tmhaigh@gmail.com>
2021-02-02 17:28:31 +1100

    made rest colour lighter

--- client/src/utils/timer/backgroundColors.js
+++ client/src/utils/timer/backgroundColors.js
@@ -2,7 +2,7 @@ export const BACKGROUND_COLORS = {
   DEFAULT: "#efeff5",
   PREPARE: "#efeff5",
   WORK: "#efeff5",
-  REST: "#ed5e5e",
+  REST: "#f5a3a3",
   BREAK: "#8cb3d9",
   COMPLETED: "#efeff5"
 }
