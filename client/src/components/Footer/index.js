9fb5a88cb344c91dd5535832db8a3ad83babf6ae
Tom Haigh <tmhaigh@gmail.com>
2021-03-14 19:36:30 +1100

    added toby face image to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import "./style.css";
+import tobyFace from '../../assets/images/toby_13.png';
 
 export default function Footer() {
   return (
     <footer>
       <div className="container">
         <div className="footer-row row">
-          <div className="links-div col s6">
-            <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-linkedin font-awesome social-icon"></i>
-            </a>
-            <a className="links-div-anchor" href="https://github.com/Conanas" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-github font-awesome social-icon"></i>
-            </a>
-          </div>
-          <div className="contact-div col s6">
-            <label>tmhaigh@gmail.com</label>
-            <label>Melbourne, Victoria, Australia</label>
+          <div className="toby-col col">
+            <img className="toby-footer" src={tobyFace} alt='toby' />
           </div>
         </div>
       </div>
