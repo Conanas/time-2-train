d52169a098a963e5b5b0ca3553071b4c69d5a738
Tom Haigh <tmhaigh@gmail.com>
2021-05-23 11:03:25 +1000

    reverted default workout

--- client/src/utils/contexts/WorkoutContext.js
+++ client/src/utils/contexts/WorkoutContext.js
@@ -5,10 +5,10 @@ const WorkoutContext = createContext();
 
 const DEFAULT_STATE = {
   title: "Quick Start",
-  reps: 3,
-  rest: 3,
-  sets: 2,
-  break: 5
+  reps: 5,
+  rest: 120,
+  sets: 1,
+  break: 0
 }
 
 const reducer = (state, action) => {
